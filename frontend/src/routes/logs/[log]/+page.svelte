579c359181a186df29f68336d0ec7a7b8e20234c
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 18:43:34 +1300

    Moving log view to other components

--- frontend/src/routes/logs/[log]/+page.svelte
+++ frontend/src/routes/logs/[log]/+page.svelte
@@ -1,10 +1,11 @@
 <script>
- import LogCard from '$components/LogCard.svelte'
- export let data;
+	import LogTable from "$components/LogTable.svelte"
+	import LineGraph from "$components/LineGraph.svelte"
+	export let data;
 </script>
 
-<div class="grid grid-cols-3 auto-rows-max gap-3">
-    {#each data.items as routine} 
-        <LogCard log={routine}/>
-    {/each}
-</div>
+
+<section class="grid grid-rows-[2fr_1fr]">
+	<LineGraph />
+	<LogTable data={data}/>
+</section>
