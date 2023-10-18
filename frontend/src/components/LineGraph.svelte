a77c70f980f366e04ec465cc24d0d03d03552fbb
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 18:59:17 +1300

    Creating line graph component

---
+++ frontend/src/components/LineGraph.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+	import * as d3 from "d3"
+
+	let vis;
+
+	// let data = [];
+	// 	for (let i = 0; i < 12; ++i) {
+	// 		data.push({date: new Date(`2023-${Math.floor(Math.random() * 13)}-${Math.floor(Math.random() * 31 )}`), value: Math.random() * 50})
+	// 	}
+	// 
+	// data.sort((a, b) => {
+	// 		if (a.date.getTime() < b.date.getTime()) {
+	// 			return -1;
+	// 		} else if (a.date.getTime() > b.date.getTime()) {
+	// 			return 1;
+	// 		}
+	// 		return 0;
+	// 	})
+	export let data = [];
+
+	if (data.length != 0) {
+		data.map(val => {
+			let date = val.date.split(" ")[0]
+			val.date = new Date(date)
+		})
+	};
+
+	let width: number;
+	let height: number;
+	const margin = {
+		top: 20,
+		right: 20,
+		bottom: 30,
+		left: 30
+	};
+
+	onMount(() => {
+		redraw();
+		window.addEventListener('resize', redraw);
+	})
+
+	function redraw(): void {
+		// empty vis div
+		d3.select(vis).html(null); 
+
+		// determine width & height of parent element and subtract the margin
+		width = d3.select(vis).node().getBoundingClientRect().width - margin.left - margin.right;
+		height = d3.select(vis).node().getBoundingClientRect().height - margin.top - margin.bottom;
+
+		let yScale = d3.scaleLinear().domain([0, 50]);
+		let xScale = d3.scaleTime([new Date("2023-01-01"), new Date("2023-12-31")], [0, width])
+
+		// init scales according to new width & height
+		xScale.range([0, width]);
+		yScale.range([height, 0]);
+
+		// create svg and create a group inside that is moved by means of margin
+		const svg = d3.select(vis)
+			.append('svg')
+			.attr('width', width + margin.left + margin.right)
+			.attr('height', height + margin.top + margin.bottom)
+			.append('g')
+			.attr('transform', `translate(${[margin.left, margin.top]})`)
+
+		// draw x and y axes
+		svg.append("g")
+			.attr("transform", `translate(${[0, height]})`)
+			.call(d3.axisBottom(xScale));
+		svg.append("g")
+    		.call(d3.axisLeft(yScale));
+
+		// draw datapoint
+		svg.append("g").selectAll("dot")
+			 .data(data)
+			 .enter()
+			 .append("circle")
+			 .attr("cx", (d) => xScale(d.date))
+			 .attr("cy", (d) => yScale(d.reps))
+			 .attr("r", 3)
+			 .style("fill", "steelblue");
+
+		// draw line
+		svg.append('path')
+			 .datum(data)
+			 .attr("fill", "none")
+			 .attr("stroke", "steelblue")
+			 .attr("stroke-width", 1.5)
+			 .attr("d", d3.line()
+										.x((d) => xScale(d.date))
+										.y((d) => yScale(d.reps)))
+		
+	}
+
+</script>
+
+<div id="vis" bind:this={vis}></div>
