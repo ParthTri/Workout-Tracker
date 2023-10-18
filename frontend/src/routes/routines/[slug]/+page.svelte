29d9c1fc8dc81945a2c75aaa997bffac55aa02e2
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 16:12:26 +1300

    Adding route to routines

---
+++ frontend/src/routes/routines/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Card } from 'flowbite-svelte';
+ export let data;
+</script>
+
+<div class="grid grid-cols-3 auto-rows-max gap-3">
+    {#each data.items as routine} 
+        {#if routine.Active == true}
+          <Card class="h-44 text-center" href="/logs/${routine.id}">
+              <h3>{routine.Name}</h3>
+              <h5>{routine.Created}</h5>
+          </Card>
+        {/if}
+    {/each}
+</div>
