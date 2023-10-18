1a73d594e6dd09a78fca58b3754e965baee9feff
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 19:44:40 +1300

    Creating component for table heading with colour indicator

---
+++ frontend/src/components/TableHeading.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Indicator } from "flowbite-svelte"
+	export let color = "";
+</script>
+
+<span class="flex items-center space-between">
+	<Indicator color={color}/>
+	<slot />
+</span>
