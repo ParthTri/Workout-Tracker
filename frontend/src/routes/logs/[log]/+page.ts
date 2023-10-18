f1c77016e7acdd89598a09b2db2fb57d1bf6d6cc
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 17:11:23 +1300

    Reformatting server function

--- frontend/src/routes/logs/[log]/+page.ts
+++ frontend/src/routes/logs/[log]/+page.ts
@@ -1,10 +1,17 @@
-
 export async function load({ fetch, params }) {
-    console.log(params.log)
-    console.log(`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(1)}'`)
-    let data = await fetch(`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(1)}'`);
-    let json = await data.json();
-    console.log(json)
+	console.log(params.log);
+	console.log(
+		`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(
+			1,
+		)}'`,
+	);
+	let data = await fetch(
+		`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(
+			1,
+		)}'`,
+	);
+	let json = await data.json();
+	console.log(json);
 
-    return json;
+	return json;
 }
