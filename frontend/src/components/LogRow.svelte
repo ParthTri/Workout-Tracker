fe850319e833965b1f8c98d7fbfae535761ffbc7
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-18 17:12:14 +1300

    Adding components for table displaying of logs

--- frontend/src/components/LogRow.svelte
+++ frontend/src/components/LogRow.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
- import { Card, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
+ import { TableBodyCell, TableBodyRow } from 'flowbite-svelte';
  export let log;
 
  const stripTime = (input: string) => {
@@ -7,19 +7,8 @@
  }
 </script>
 
-<Card class="h-44 text-center" href="/logs/${log.id}">
-    <h3>{stripTime(log.date)}</h3>
-    <h5>{stripTime(log.created)}</h5>
-    <TableBody>
-        <TableHead>
-            <TableHeadCell>Sets</TableHeadCell>
-            <TableHeadCell>Reps</TableHeadCell>
-        </TableHead>
-        <TableBody>
-            <TableBodyRow>
-                <TableBodyCell>{log.sets}</TableBodyCell>
-                <TableBodyCell>{log.reps}</TableBodyCell>
-            </TableBodyRow>
-        </TableBody>
-    </TableBody>
-</Card>
+<TableBodyRow class="h-4">
+    <TableBodyCell>{stripTime(log.date)}</TableBodyCell>
+    <TableBodyCell>{log.sets}</TableBodyCell>
+    <TableBodyCell>{log.reps}</TableBodyCell>
+</TableBodyRow>
---
+++ frontend/src/components/LogTable.svelte
@@ -0,0 +1,25 @@
+<script>
+	import LogRow from '$components/LogRow.svelte'
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+	} from "flowbite-svelte";
+
+	export let data;
+</script>
+<Table class="w-full">
+	<TableHead >
+		<TableHeadCell>Date</TableHeadCell>
+		<TableHeadCell>Sets</TableHeadCell>
+		<TableHeadCell>Reps</TableHeadCell>
+	</TableHead>
+	<TableBody>
+		{#each data.items as routine} 
+			<LogRow log={routine}/>
+		{/each}
+	</TableBody>
+</Table>
