6452879fc72858267930ec8144d71994f26b87de
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-21 17:53:21 +1300

    Reformatting

--- frontend/src/routes/routines/+page.ts
+++ frontend/src/routes/routines/+page.ts
@@ -1,8 +1,8 @@
-
 export async function load({ fetch, params }) {
-    let data = await fetch("http://127.0.0.1:8090/api/collections/Routine/records");
-    let json = await data.json();
-    console.log(json)
+	let data = await fetch(
+		"http://127.0.0.1:8090/api/collections/Routine/records",
+	);
+	let json = await data.json();
 
-    return json;
+	return json;
 }
