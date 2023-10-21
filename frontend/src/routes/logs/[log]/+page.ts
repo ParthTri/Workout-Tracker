da60f7ebbcf473fcd4bafb69cf29913bf5e55ce5
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-21 17:57:25 +1300

    Removing console logs

--- frontend/src/routes/logs/[log]/+page.ts
+++ frontend/src/routes/logs/[log]/+page.ts
@@ -1,10 +1,4 @@
 export async function load({ fetch, params }) {
-	console.log(params.log);
-	console.log(
-		`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(
-			1,
-		)}'`,
-	);
 	let data = await fetch(
 		`http://127.0.0.1:8090/api/collections/Log/records?filter=ExcerciseID='${params.log.substring(
 			1,
