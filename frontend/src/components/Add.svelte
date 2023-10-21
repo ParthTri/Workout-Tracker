c454e2aee3b15239e1353004ceaccda32865270d
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-21 18:31:53 +1300

    Adding button to add logs

---
+++ frontend/src/components/Add.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<div class="absolute bottom-[10px] right-[12px] w-12 h-12 border-2 border-black rounded-full p-2 hover:cursor-pointer" on:click={() => console.log("clicked")}>
+	<img alt="+" src="/plus-black.png"/>
+</div>
+<!-- <button class="" ></button> -->
--- frontend/src/routes/logs/[log]/+page.svelte
+++ frontend/src/routes/logs/[log]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import LogTable from "$components/LogTable.svelte"
 	import LineGraph from "$components/LineGraph.svelte"
+	import Add from "$components/Add.svelte"
 	export let data;
 </script>
 
@@ -8,4 +9,5 @@
 <section class="grid grid-rows-[2fr_1fr]">
 	<LineGraph data={data.items}/>
 	<LogTable data={data}/>
+	<Add />
 </section>
