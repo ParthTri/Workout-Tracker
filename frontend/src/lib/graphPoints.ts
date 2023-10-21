632858ec058b6e92f5eda521cf557206581ba58f
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-21 18:00:23 +1300

    Adding library for graphing points and lines

---
+++ frontend/src/lib/graphPoints.ts
@@ -0,0 +1,47 @@
+import * as d3 from "d3";
+import type { ScaleLinear } from "d3";
+
+export function drawPoints(
+	graph: ScaleLinear,
+	color: string,
+	data: Array<any>,
+	xScale: any,
+	yScale: any,
+	key: string,
+): void {
+	graph
+		.append("g")
+		.selectAll("dot")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx", (d) => xScale(d.date))
+		.attr("cy", (d) => yScale(d[key]))
+		.attr("r", 3)
+		.style("fill", color);
+
+	return graph;
+}
+
+export function drawLine(
+	graph: ScaleLinear,
+	color: string,
+	data: Array<any>,
+	xScale: any,
+	yScale: any,
+	key: string,
+): void {
+	graph
+		.append("path")
+		.datum(data)
+		.attr("fill", "none")
+		.attr("stroke", color)
+		.attr("stroke-width", 1.5)
+		.attr(
+			"d",
+			d3
+				.line()
+				.x((d) => xScale(d.date))
+				.y((d) => yScale(d[key])),
+		);
+}
