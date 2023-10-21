d6d054b0a34ccac5ec6b03d4a5b0c9076c001fd5
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-21 18:05:06 +1300

    Reformatting server fetching

--- frontend/src/routes/routines/[slug]/+page.ts
+++ frontend/src/routes/routines/[slug]/+page.ts
@@ -1,9 +1,8 @@
-
 export async function load({ fetch, params }) {
-    console.log(`http://127.0.0.1:8090/api/collections/Excercise/records`)
-    let data = await fetch(`http://127.0.0.1:8090/api/collections/Excercise/records`);
-    let json = await data.json();
-    console.log(json)
+	let data = await fetch(
+		`http://127.0.0.1:8090/api/collections/Excercise/records`,
+	);
+	let json = await data.json();
 
-    return json;
+	return json;
 }
