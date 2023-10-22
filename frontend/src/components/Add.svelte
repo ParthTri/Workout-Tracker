1f771541f027c716892a128edd8d274cbca0e9f9
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-22 15:35:12 +1300

    Toggling display of AddCard

--- frontend/src/components/Add.svelte
+++ frontend/src/components/Add.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
+	export let toggleShow;
 
 </script>
 
-<div class="absolute bottom-[10px] right-[12px] w-12 h-12 border-2 border-black rounded-full p-2 hover:cursor-pointer" on:click={() => console.log("clicked")}>
+<div class="absolute bottom-[10px] right-[12px] w-12 h-12 border-2 border-black rounded-full p-2 hover:cursor-pointer" on:click={toggleShow}>
 	<img alt="+" src="/plus-black.png"/>
 </div>
 <!-- <button class="" ></button> -->
--- frontend/src/routes/logs/[log]/+page.svelte
+++ frontend/src/routes/logs/[log]/+page.svelte
@@ -2,12 +2,24 @@
 	import LogTable from "$components/LogTable.svelte"
 	import LineGraph from "$components/LineGraph.svelte"
 	import Add from "$components/Add.svelte"
+	import AddCard from "$components/AddCard.svelte"
+
 	export let data;
+
+	let show = true;
+	const toggleShow = () => show = !show;
+	let excerciseID = data.items[0].ExcerciseID;
 </script>
 
 
 <section class="grid grid-rows-[2fr_1fr]">
 	<LineGraph data={data.items}/>
 	<LogTable data={data}/>
-	<Add />
+	<Add {toggleShow}/>
+
+	{#if show}
+		<AddCard {excerciseID}/>
+	{/if}
+
 </section>
+
