ac945210caec2967f562ba6e3fdf01e87d1bcd05
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-27 11:10:01 +1300

    Building out routine page for creating new routines

--- frontend/src/routes/routines/+page.svelte
+++ frontend/src/routes/routines/+page.svelte
@@ -1,19 +1,40 @@
-<script>
-    import { Card } from 'flowbite-svelte';
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+
 	import { stripTime } from "$lib/formatting.ts"
-    export let data;
+	import Add from "$components/Add.svelte"
+	import RoutineCard from '$components/formCards/RoutineCard.svelte';
+
+	import type { RoutineData } from "$lib/interfaces.ts"
+
+	export let data;
+	data = data.items;
+
+	let show: boolean = false;
+	const toggleShow = () => show = !show;
+
+	const addData = (log: RoutineData) => {
+		data.push(log);
+		data = data;
+	}
 </script>
 
 <svelte:head>
-    <title>Routines</title>
+	<title>Routines</title>
 </svelte:head>
 
 
 <div class="grid grid-cols-3 auto-rows-max gap-3">
-    {#each data.items as routine} 
-        <Card class="h-44 text-center" href="/routines/${routine.id}">
-            <h3>{routine.name}</h3>
-            <h5>{stripTime(routine.created)}</h5>
-        </Card>
-    {/each}
+	{#each data as routine} 
+		<Card class="h-44 text-center" href="/routines/${routine.id}">
+		<h3>{routine.name}</h3>
+		<h5>{stripTime(routine.created)}</h5>
+		</Card>
+	{/each}
+
+	<Add {toggleShow}/>
+
+	{#if show}
+		<RoutineCard {toggleShow} updateState={addData}/>
+	{/if}
 </div>
