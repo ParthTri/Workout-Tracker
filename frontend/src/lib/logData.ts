d6e08937426c9ae74f49ebb2370c2c24100a4782
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-26 14:26:49 +1300

    Reformatting logging functions to work independently

--- frontend/src/lib/logData.ts
+++ frontend/src/lib/logData.ts
@@ -6,20 +6,22 @@ interface LogData {
 	weight: number;
 }
 
-function extractData(form: HTMLFormElement): LogData {
+export function extractData(form: HTMLFormElement): LogData {
 	let x: LogData = {
 		ExcerciseID: 0,
 		date: "",
 		sets: 0,
 		reps: 0,
-		weight: 0,
+		weight: -1,
 	};
 
-	for (let i = 0; i < form.length; i++) {
-		if (form[i].name == "date") {
-			x[form[i].name] = new Date(form[i].value).toISOString().replace("T", " ");
+	for (let i = 0; i < form.target.length; i++) {
+		if (form.target[i].name == "date") {
+			x[form.target[i].name] = new Date(form.target[i].value)
+				.toISOString()
+				.replace("T", " ");
 		} else {
-			x[form[i].name] = form[i].value;
+			x[form.target[i].name] = form.target[i].value;
 		}
 	}
 
