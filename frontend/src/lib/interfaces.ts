3265be4cadfc1aed11383977c2f3a14ddb9415aa
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-26 15:53:17 +1300

    Setting up different interfaces

---
+++ frontend/src/lib/interfaces.ts
@@ -0,0 +1,14 @@
+export interface LogData {
+	ExcerciseID: number;
+	date: string;
+	sets: number;
+	reps: number;
+	weight: number;
+}
+
+export interface ExcerciseData {
+	RoutineID: string;
+	Date: string;
+	Name: string;
+	Weight: boolean;
+}
