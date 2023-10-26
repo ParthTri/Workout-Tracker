afd9c08a862891cee3cca6e18b7e525c43632f4f
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-26 14:25:03 +1300

    Styling add button

--- frontend/src/components/Add.svelte
+++ frontend/src/components/Add.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let toggleShow;
-
 </script>
 
-<div class="absolute bottom-[10px] right-[12px] w-12 h-12 border-2 border-black rounded-full p-2 hover:cursor-pointer" on:click={toggleShow}>
-	<img alt="+" src="/plus-black.png"/>
+<div
+	class="absolute bottom-[10px] right-[12px] w-12 h-12 border-2 border-black rounded-full p-2 hover:cursor-pointer"
+	on:click="{toggleShow}"
+>
+	<img alt="+" src="/plus-black.png" />
 </div>
-
