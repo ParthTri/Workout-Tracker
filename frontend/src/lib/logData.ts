75c753edeaa6982250a54b484c65afbcd974a9aa
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-26 15:53:06 +1300

    Removing interfaces from logData.ts

--- frontend/src/lib/logData.ts
+++ frontend/src/lib/logData.ts
@@ -1,31 +1,21 @@
-interface LogData {
-	ExcerciseID: number;
-	date: string;
-	sets: number;
-	reps: number;
-	weight: number;
-}
+import type { LogData } from "./interfaces.ts";
 
-export function extractData(form: HTMLFormElement): LogData {
-	let x: LogData = {
-		ExcerciseID: 0,
-		date: "",
-		sets: 0,
-		reps: 0,
-		weight: -1,
-	};
-
+export function extractData(form: HTMLFormElement, obj: any): void {
 	for (let i = 0; i < form.target.length; i++) {
 		if (form.target[i].name == "date") {
-			x[form.target[i].name] = new Date(form.target[i].value)
+			obj[form.target[i].name] = new Date(form.target[i].value)
 				.toISOString()
 				.replace("T", " ");
 		} else {
-			x[form.target[i].name] = form.target[i].value;
+			if (form.target[i].type == "checkbox") {
+				obj[form.target[i].name] = form.target[i].checked;
+			} else {
+				obj[form.target[i].name] = form.target[i].value;
+			}
 		}
 	}
 
-	return x;
+	delete obj[""];
 }
 
 export default async function logData(
@@ -44,7 +34,7 @@ export default async function logData(
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(extractedData),
+			body: JSON.stringify(log),
 		},
 	);
 
