1e22dd4bb1674efb19acbb5db01134eacf55dda7
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-27 11:10:16 +1300

    Creating card component for create routines

---
+++ frontend/src/components/formCards/RoutineCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+
+	import FormLabel from "./FormLabel.svelte"
+	import Button from "./Button.svelte"
+
+	import type { RoutineData } from "$lib/interfaces.ts"
+	import { extractData } from "$lib/logData.ts";
+
+	export let updateState: (val: any) => void;
+	export let toggleShow : () => void;
+
+	const handleSubmission = (event: HTMLFormElement) => {
+		let tmp = {} as RoutineData;
+		extractData(event, tmp);
+
+		let formData: RoutineData = {
+			name: tmp.name,
+		}
+		
+		updateState(formData);
+	}
+</script>
+
+<Card class="min-h-fit absolute left-2/4 top-2/4 translate-x-[-50%] translate-y-[-50%] w-96 drop-shadow-2xl">
+	<form class="h-full grid grid-cols-1 grid-rows-auto gap-4" on:submit|preventDefault={(e) => handleSubmission(e)}>
+	<FormLabel name="name" label="Routien Name">
+		<input name="name" type="text" value=""/>
+	</FormLabel>
+
+	<Button>Create</Button>
+	</form>
+</Card>
