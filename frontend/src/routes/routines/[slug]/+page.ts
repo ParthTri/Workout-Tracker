6908782706df1ab14a8eafc6e4de9ed4ddc654bf
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-27 14:38:49 +1300

    Getting excercises for a specific routine

--- frontend/src/routes/routines/[slug]/+page.ts
+++ frontend/src/routes/routines/[slug]/+page.ts
@@ -2,7 +2,7 @@
 
 export async function load({ fetch, params }) {
 	let data = await fetch(
-		`http://127.0.0.1:8090/api/collections/Excercise/records`,
+		`http://127.0.0.1:8090/api/collections/Excercise/records?filter=(RoutineID='${params.slug.substring(1)}')`,
 	);
 	let json = await data.json();
 
