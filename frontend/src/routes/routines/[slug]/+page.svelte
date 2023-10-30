769f2761df496a07e4554ad22bb5aa6e41d49e29
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-10-30 16:02:06 +1300

    Adding error handling to routines
    Error handeling if no excercises are found with specific RoutineID

--- frontend/src/routes/routines/[slug]/+page.svelte
+++ frontend/src/routes/routines/[slug]/+page.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
 	import { Card } from 'flowbite-svelte';
+	import {page} from "$app/stores";
 
 	import Add from "$components/Add.svelte"
 	import ExcerciseCard from "$components/formCards/ExcerciseCard.svelte"
@@ -12,23 +13,34 @@
 
 	const addData = (log) => {
 		data.push(log);
-		data = data
+		data = data;
 	}
 
-	let routineID = data[0].RoutineID;
+	let routineID: string;
+	try {
+		 routineID = data[0].RoutineID;
+	} catch {
+		routineID = $page.url.pathname.split("/").pop().substring(1)
+	}
+
 	let show = false;
 	const toggleShow = () => show = !show;
 </script>
 
 <div class="grid grid-cols-3 auto-rows-max gap-3">
-    {#each data as routine} 
-        {#if routine.Active == true}
-          <Card class="h-44 text-center" href="/logs/${routine.id}">
-              <h3>{routine.Name}</h3>
-              <h5>{stripTime(routine.Created)}</h5>
-          </Card>
-        {/if}
-    {/each}
+		{#if data.length > 0}
+			{#each data as routine} 
+					{#if routine.Active == true}
+						<Card class="h-44 text-center" href="/logs/${routine.id}">
+								<h3>{routine.Name}</h3>
+								<h5>{stripTime(routine.Created)}</h5>
+						</Card>
+					{/if}
+			{/each}
+		{:else}
+			<p>Nothing here</p>		
+		
+		{/if}
 
 		<Add {toggleShow}/>
 
