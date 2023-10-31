1eceb39ef95176bb992ffebd47a0b88e4c94016a
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-01 11:59:19 +1300

    Moving bottom nav outside of main and making it fixed to the bottom of the viewport

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -19,19 +19,20 @@
 <main>
 	<slot />
 
-	<BottomNav position="absolute" navType="group" classInner="grid-cols-4">
-		<BottomNavItem href="/">
-			<HomeSolid />
-		</BottomNavItem>
-
-		<BottomNavItem href="/routines">
-			<GridSolid />
-		</BottomNavItem>
-
-		<BottomNavItem hrerf="/settings">
-			<AdjustmentsVerticalSolid />
-		</BottomNavItem>
-
-	</BottomNav>
-
 </main>
+
+<BottomNav navType="group" classInner="bottom-0 left-0 grid-cols-4">
+	<BottomNavItem href="/">
+		<HomeSolid />
+	</BottomNavItem>
+
+	<BottomNavItem href="/routines">
+		<GridSolid />
+	</BottomNavItem>
+
+	<BottomNavItem hrerf="/settings">
+		<AdjustmentsVerticalSolid />
+	</BottomNavItem>
+
+</BottomNav>
+
