70f799feebb37e9931b86f1e18d56ad9012f63e5
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-03 10:38:37 +1300

    Implmenting linegraph in statlog presenting

---
+++ frontend/src/routes/stats/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import LineGraph from "$components/LineGraph.svelte";
+
+	export let data: any;
+	let dataItems = data.items;
+
+	let statId = data.stat.collectionId;
+	console.log(data.stat)
+	let targetAndColour: Array<Array<string>> = [[data.stat.Unit, "steelblue"]]
+</script>
+
+<svelte:head>
+	<title>{data.stat.Name}</title>
+</svelte:head>
+
+<div>
+	<LineGraph data={dataItems} showWeight={false} excerciseID={statId} {targetAndColour}/>
+</div>
