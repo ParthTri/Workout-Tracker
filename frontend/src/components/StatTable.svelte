33356ee969ec65a30de891ed11a317175336f40d
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-06 13:43:39 +1300

    Fixing data reading

--- frontend/src/components/StatTable.svelte
+++ frontend/src/components/StatTable.svelte
@@ -10,7 +10,6 @@
 
 	export let data: Array<any>;
 	export let targetAndColour: Array<Array<string>>;
-	console.log(targetAndColour[0][1])
 </script>
 <Table class="w-full">
 	<TableHead >
@@ -18,7 +17,7 @@
 		<TableHeadCell><TableHeading color={targetAndColour[0][1]}>{targetAndColour[0][0]}</TableHeading></TableHeadCell>
 	</TableHead>
 	<TableBody>
-		{#each data.items as log} 
+		{#each data as log} 
 			<StatRow {log}/>
 		{/each}
 	</TableBody>
