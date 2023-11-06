e25b372dcd78a58ab479aa9c6db5b8a59ff38438
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-06 14:22:42 +1300

    Creating a EditableCard component

---
+++ frontend/src/components/EditableCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Card, Dropdown, DropdownItem, Button } from "flowbite-svelte"
+	import { DotsHorizontalOutline } from "flowbite-svelte-icons";
+
+	export let innerClass: string;
+	export let href: string;
+
+	let style = "w-80 h-44 text-center"
+	style += ` ${innerClass}`
+</script>
+
+
+<Card {href} class={style}>
+	<!-- <div class="flex justify-end"> -->
+
+		<!-- <DotsHorizontalOutline> -->
+		<!-- <Dropdown class="w-36"> -->
+		<!-- 	<DropdownItem>Test</DropdownItem> -->
+		<!-- 	<DropdownItem></DropdownItem> -->
+		<!-- 	<DropdownItem></DropdownItem> -->
+		<!-- </Dropdown> -->
+
+	<!-- </div> -->
+	<slot></slot>
+</Card>
