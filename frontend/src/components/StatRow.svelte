0f981525eb67754a7f1fa58be2a7b990f4e277bb
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-06 13:43:44 +1300

    Adding types

--- frontend/src/components/StatRow.svelte
+++ frontend/src/components/StatRow.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
  import { TableBodyCell, TableBodyRow } from 'flowbite-svelte';
 import { stripTime } from "$lib/formatting.ts"
- export let log;
+import type { StatLogData } from "$lib/interfaces.ts"
 
+ export let log: StatLogData;
 </script>
 
 <TableBodyRow class="h-4">
