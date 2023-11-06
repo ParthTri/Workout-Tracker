4e90f3c9bd2818d06f8238d7ba81ee24e614b438
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-06 13:43:25 +1300

    Implmenting stat creation

--- frontend/src/routes/stats/[slug]/+page.svelte
+++ frontend/src/routes/stats/[slug]/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import LineGraph from "$components/LineGraph.svelte";
 	import StatTable from "$components/StatTable.svelte";
+	import Add from "$components/Add.svelte";
+	import StatLogCard from "$components/formCards/StatLogCard.svelte";
+
+	import type { StatLogData } from "$lib/interfaces";
 
 	export let data: any;
-	let dataItems = data.items;
+	let dataItems: Array<StatLogData> = data.items;
 
 	let statId = data.stat.collectionId;
 	let targetAndColour: Array<Array<string>> = [[data.stat.Unit, "blue"]]
+
+	let show = false;
+	const toggleShow = () => show = !show;
+
+	const updateState = (log: StatLogData) => {
+		dataItems.push(log);
+		dataItems = dataItems
+	}
+
 </script>
 
 <svelte:head>
@@ -16,5 +29,11 @@
 <div>
 	<LineGraph data={dataItems} showWeight={false} excerciseID={statId} {targetAndColour}/>
 
-	<StatTable {data} {targetAndColour}/>
+	<StatTable data={dataItems} {targetAndColour}/>
+
+	<Add {toggleShow}/>
+
+	{#if show}
+		<StatLogCard {toggleShow} {updateState}/>	
+	{/if}
 </div>
