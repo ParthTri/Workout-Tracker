99e996b7013fdead43f40cc4b41c60242ee22b17
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-19 12:13:03 +1300

    Adding dropdown menu to Editable card

--- frontend/src/components/EditableCard.svelte
+++ frontend/src/components/EditableCard.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
-	import { Card, Dropdown, DropdownItem, Button } from "flowbite-svelte"
-	import { DotsHorizontalOutline } from "flowbite-svelte-icons";
+	import  Dropdown  from "./DropDown/Dropdown.svelte"
+	import  DropdownItem  from "./DropDown/DropdownItem.svelte"
 
-	export let innerClass: string;
+	// export let innerClass: string;
 	export let href: string;
 
-	let style = "w-80 h-44 text-center"
-	style += ` ${innerClass}`
+	let style = "w-80 h-44 text-center border-2 rounded-xl p-2"
+	// style += ` ${innerClass}`
+	
+	let dropDownOpen = false;
+	let dropDownClosed = true;
+
+	const updateDropDown = (val: boolean) => {
+		dropDownOpen = val;
+	}
+
+	const handleEvent = (e: MouseEvent) => {
+		if (!dropDownOpen && dropDownClosed) {
+			console.log(`Navigate to ${href}`)
+		} else {
+			dropDownClosed = !dropDownClosed;
+		}
+	}
 </script>
 
 
-<Card {href} class={style}>
-	<!-- <div class="flex justify-end"> -->
-
-		<!-- <DotsHorizontalOutline> -->
-		<!-- <Dropdown class="w-36"> -->
-		<!-- 	<DropdownItem>Test</DropdownItem> -->
-		<!-- 	<DropdownItem></DropdownItem> -->
-		<!-- 	<DropdownItem></DropdownItem> -->
-		<!-- </Dropdown> -->
-
-	<!-- </div> -->
-	<slot></slot>
-</Card>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class={style} on:click={(e) => handleEvent(e)}>
+	<div class="flex justify-end"> 
+		<Dropdown {updateDropDown}>
+				<DropdownItem action={() => console.log("rename")}>Rename</DropdownItem>	
+				<DropdownItem action={() => console.log("archive")}>Archive</DropdownItem>	
+				<DropdownItem action={() => console.log("delete")}>Delete</DropdownItem>	
+		</Dropdown>
+	</div>
+	<div class="wrapper">
+		<slot></slot>
+	</div>
+</div>
