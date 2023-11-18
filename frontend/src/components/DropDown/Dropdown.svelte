a4fc448a47f885ede383018ffe56302865680732
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-19 12:12:51 +1300

    Creating custom dropdown menu

---
+++ frontend/src/components/DropDown/Dropdown.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { DotsHorizontalOutline } from "flowbite-svelte-icons";
+	export let updateDropDown: (val: boolean) => void;
+
+	let visible = false;
+	const handleClick = () => {
+		visible = !visible;
+		updateDropDown(visible);
+		console.log(visible)
+	}
+</script>
+
+<div>
+	<button on:click={() => handleClick()}>
+		<DotsHorizontalOutline />	
+	</button>
+
+	{#if visible}
+		<ul class="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 p-2">
+			<slot></slot>
+		</ul>		
+	{/if}
+</div>
---
+++ frontend/src/components/DropDown/DropdownItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let action: () => void;
+</script>
+
+<li>
+	<button on:click={action}>
+		<slot></slot>
+	</button>
+</li>
