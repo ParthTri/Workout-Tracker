94974d9e46b2cdb85e94ce7c1667e8ded8ee4395
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-11-30 16:38:21 +1300

    Restructuring notification behaviour

--- frontend/src/components/Notification.svelte
+++ frontend/src/components/Notification.svelte
@@ -1,40 +1,27 @@
-<script>
+<script lang="ts">
 	import { Toast } from "flowbite-svelte";
 	import { CheckCircleSolid, CloseCircleSolid } from "flowbite-svelte-icons";
-	export let success = 0;
-</script>
+	export let success: boolean = true;
+	export let toggleNotify: () => void;
+	</script>
 
-{#if success != 0}
-	<div class="">
-		{#if success == 1}
-			<Toast color="green">
-				<svelte:fragment slot="icon">
-					<CheckCircleSolid class="w-5 h-5" />
-					<span class="sr-only">Check icon</span>
-				</svelte:fragment>
-				Item moved successfully.
-			</Toast>
-		{:else if success == -1}
-			<Toast color="red">
-				<svelte:fragment slot="icon">
-					<CloseCircleSolid class="w-5 h-5" />
-					<span class="sr-only">Error icon</span>
-				</svelte:fragment>
-				Log couldn't be entered.
-			</Toast>
-		{/if}
-	</div>
+<div class="fixed top-0 left-2/4 translate-x-[-50%]">
+{#if success}
+	<Toast on:close={toggleNotify}>
+		<svelte:fragment slot="icon">
+			<CheckCircleSolid class="w-5 h-5 text-green-300" />
+			<span class="sr-only">Check icon</span>
+		</svelte:fragment>
+		<slot name="sucess"/>
+	</Toast>	
+{:else}
+	<Toast on:close={toggleNotify}>
+		<svelte:fragment slot="icon">
+			<CloseCircleSolid class="w-5 h-5" />
+			<span class="sr-only">Error icon</span>
+		</svelte:fragment>
+		<slot name="fail"/>
+	</Toast>	
 {/if}
 
-<style>
-	.notification {
-	}
-
-	.success {
-		background-color: green;
-	}
-
-	.failure {
-		background-color: red;
-	}
-</style>
+</div>
